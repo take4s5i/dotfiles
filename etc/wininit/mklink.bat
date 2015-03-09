d6ad0cf62fe8ebb8d70d93fe1143550b4c8a115f
take4s5i <take4s5i.0000@gmail.com>
2015-03-09 10:53:39 +0900

    add mklink.bat

---
+++ etc/wininit/mklink.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+
+rem TODO : 絶対パスではなく、リポジトリの相対パスに書き換える
+del %USERPROFILE%\.bash_profile
+del %USERPROFILE%\.gitconfig   
+del %USERPROFILE%\.gitignore   
+del %USERPROFILE%\.gvimrc      
+del %USERPROFILE%\.inputrc     
+del %USERPROFILE%\.vimrc       
+
+mklink %USERPROFILE%\.bash_profile %USERPROFILE%\projects\github\dotfiles\.bash_profile 
+mklink %USERPROFILE%\.gitconfig    %USERPROFILE%\projects\github\dotfiles\.gitconfig
+mklink %USERPROFILE%\.gitignore    %USERPROFILE%\projects\github\dotfiles\.gitignore
+mklink %USERPROFILE%\.gvimrc       %USERPROFILE%\projects\github\dotfiles\.gvimrc
+mklink %USERPROFILE%\.inputrc      %USERPROFILE%\projects\github\dotfiles\.inputrc
+mklink %USERPROFILE%\.vimrc        %USERPROFILE%\projects\github\dotfiles\.vimrc
